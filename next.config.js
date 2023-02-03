79093dcf3a7d2bfff5d61e964272fde6b6d73be5
mfernandezlop <46795055+mfernandezlop@users.noreply.github.com>
2023-02-03 01:39:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: [
       'avatars.githubusercontent.com',
